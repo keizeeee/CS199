ef1637b3f3eb521beb70c7b032de7c397570c7b4
keizeeee <99263770+keizeeee@users.noreply.github.com>
2025-02-16 22:36:06 +0800

    build

---
+++ geospatial-app/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [MatSlideToggleModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
