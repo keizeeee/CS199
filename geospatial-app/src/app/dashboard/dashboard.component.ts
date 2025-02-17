e858b89c0ce44fe8583d3c3d81351c0b20f95acd
keizeeee <99263770+keizeeee@users.noreply.github.com>
2025-02-17 16:01:04 +0800

    feat: show map on screen

--- geospatial-app/src/app/dashboard/dashboard.component.ts
+++ geospatial-app/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import * as L from 'leaflet'
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +8,26 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+  ngOnInit(): void{
+    this.configMap()
+  }
+
+  // must define map variable
+  map : any
+
+  configMap(){
+    this.map = L.map('map',{
+      center: [12.8797, 121.7740], // Philippines
+      zoom: 6,
+    }); // id is map in dashboard
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { // set up map
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+
+
+  }
+  
 }
